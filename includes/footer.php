3325d5e2541ccad33a55ce8d8191583a2a8c1336
Fabian <f.montemayor26@gmail.com>
2016-11-16 12:43:24 -0600

    About us

--- includes/footer.php
+++ includes/footer.php
@@ -5,7 +5,7 @@
                 <div class="mdl-logo">Traveasy</div>
                 <ul class="mdl-mini-footer__link-list">
                     <li><a href="index.php">Home</a></li>
-                    <li><a href="#">About Us</a></li>
+                    <li><a href="about.php">About Us</a></li>
                     <li><a href="contact.php">Contact Us</a></li>
                 </ul>
             </div>
