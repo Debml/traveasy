c797d4c325264b98b5ce8d5431c37890ac54f880
David Benitez <debml700@gmail.com>
2018-02-10 15:12:30 -0600

    - design for sign in screen

--- app/javascript/components/user_sign_in.jsx
+++ app/javascript/components/user_sign_in.jsx
@@ -7,7 +7,7 @@ export default class SignIn extends React.Component {
             method: 'POST',
             url: '/users/sign_in',
             data: {user: {
-                email: document.getElementById("username").value,
+                email: document.getElementById("email").value,
                 password: document.getElementById("password").value,
             }},
             headers: {
@@ -21,21 +21,24 @@ export default class SignIn extends React.Component {
     
     render() {
         return(
-            <form className="coloredBorder">
-                <h3>Log In</h3>
-                <div className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-                    <input className="mdl-textfield__input" type="text" id="username"/>
-                    <label className="mdl-textfield__label" htmlFor="username">Username</label>
-                </div><br/>
+            <form className="sign_in-form">
+                <h3 className="sign_in-form__title">Sign in</h3>
+                
+                <div className="sign_in-form__input-container">
+                    <i className="material-icons sign_in-form__input-icon">email</i>
+                    <input className="mdl-textfield__input sign_in-form__input" type="text" id="email" placeholder="Email"/>
+                </div>
 
-                <div className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-                    <input className="mdl-textfield__input" type="password" id="password"/>
-                    <label className="mdl-textfield__label" htmlFor="password">Password</label>
-                </div><br/>
+                <div className="sign_in-form__input-container">
+                    <i className="material-icons sign_in-form__input-icon">lock</i>
+                    <input className="mdl-textfield__input sign_in-form__input" type="password" id="password" placeholder="Password"/>
+                </div>
 
-                <button onClick={this.signIn.bind(this)} type="button" className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent" id="loginButton">
-                Login
+                <button onClick={this.signIn.bind(this)} type="button" className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--colored sign_in-form__button" id="sign_in-button">
+                    Sign in
                 </button>
+                
+                <div className="sign_in-form__sign_up-container">Not registered? <span className="sign_in-form__sign_up-button">Sign up</span></div>
             </form>
         );
     }
--- app/javascript/packs/navigation.js
+++ app/javascript/packs/navigation.js
@@ -4,5 +4,5 @@ import { render } from 'react-dom';
 import MainNav from '../components/nav_main_nav';
 import Drawer from '../components/nav_drawer';
 
-render(<MainNav loggedIn = {false}/>, document.getElementById("bodiv"))
+//render(<MainNav loggedIn = {false}/>, document.getElementById("bodiv"))
 //render(<Drawer loggedIn = {false}/>, document.getElementById("bodiv"))
